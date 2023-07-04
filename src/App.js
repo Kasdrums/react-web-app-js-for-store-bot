cab88835e01c09997867c76d1ea26c3356409742
Alexander <thelegindrum@gmail.com>
2023-07-05 00:34:33 +0300

    add commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,12 @@ import { useEffect } from 'react'
 import './App.css'
 
 const tg = window.Telegram.WebApp
-
 function App() {
 	useEffect(() => {
 		tg.ready()
 	}, [])
 
-	const onClose = () => {
-		tg.close()
-	}
-
-	return (
-		<div className="App">
-			work!!!!
-			<button onClick={onClose}>Закрыть</button>
-		</div>
-	)
+	return <div className="App">work!!!!</div>
 }
 
 export default App
