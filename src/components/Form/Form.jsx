119486cc7baf6248e6a7e4b22d2ab26ba0ebcd20
Alexander <thelegindrum@gmail.com>
2023-07-05 01:29:07 +0300

    add commit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import './Form.css'
 import { useTelegram } from '../../hooks/useTelegram'
 
@@ -7,6 +7,36 @@ const Form = () => {
 	const [street, setStreet] = useState('')
 	const [subject, setSubject] = useState('physical')
 	const { tg } = useTelegram()
+
+	const onSendData = useCallback(() => {
+		const data = {
+			country,
+			street,
+			subject,
+		}
+		tg.sendData(JSON.stringify(data))
+	}, [country, street, subject])
+
+	useEffect(() => {
+		tg.onEvent('mainButtonClicked', onSendData)
+		return () => {
+			tg.offEvent('mainButtonClicked', onSendData)
+		}
+	}, [onSendData])
+
+	useEffect(() => {
+		tg.MainButton.setParams({
+			text: 'Отправить данные',
+		})
+	}, [])
+
+	useEffect(() => {
+		if (!street || !country) {
+			tg.MainButton.hide()
+		} else {
+			tg.MainButton.show()
+		}
+	}, [country, street])
 
 	const onChangeCountry = (e) => {
 		setCountry(e.target.value)
