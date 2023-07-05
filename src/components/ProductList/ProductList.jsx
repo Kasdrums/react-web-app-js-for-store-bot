af4018010b13e9c91c5121a13dafd6243f27114e
Alexander <thelegindrum@gmail.com>
2023-07-05 04:27:50 +0300

    add commit

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import './ProductList.css'
 import ProductItem from '../ProductItem/ProductItem'
 import { useTelegram } from '../../hooks/useTelegram'
 
+//товары перенесем в БД!
 const products = [
 	{
 		id: '1',
@@ -61,7 +62,30 @@ const getTotalPrice = (items = []) => {
 }
 
 const ProductList = () => {
-	const { tg } = useTelegram()
+	const [addedItems, setAddedItems] = useState([])
+	const { tg, queryId } = useTelegram()
+
+	const onSendData = useCallback(() => {
+		const data = {
+			products: addedItems,
+			totalPrice: getTotalPrice(addedItems),
+			queryId,
+		}
+		fetch('http://localhost:3000', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		})
+	}, [addedItems])
+
+	useEffect(() => {
+		tg.onEvent('mainButtonClicked', onSendData)
+		return () => {
+			tg.offEvent('mainButtonClicked', onSendData)
+		}
+	}, [onSendData])
 
 	const onAdd = (product) => {
 		const alreadyAdded = addedItems.find((item) => item.id === product.id)
